bf212988136b9f42be15558865c26a3c4809037b
Paulo Risucci <paulo.risucci@academico.ifpb.edu.br>
2023-09-25 23:57:47 -0300

    First commit

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -3,27 +3,53 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import java.nio.charset.StandardCharsets;
+
 
 public class Consumidor {
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("mqadmin");
+        connectionFactory.setPassword("Admin123XX_");
+
         Connection conexao = connectionFactory.newConnection();
         Channel canal = conexao.createChannel();
+        int prefetchCount = 1;
+        canal.basicQos(prefetchCount);
 
-        String NOME_FILA = "plica"
+        String NOME_FILA = "fila_de_tarefas"
                 + "";
-        canal.queueDeclare(NOME_FILA, false, false, false, null);
+        boolean duravel = true;
+        canal.queueDeclare(NOME_FILA, duravel, false, false, null);
 
         DeliverCallback callback = (consumerTag, delivery) -> {
-            String mensagem = new String(delivery.getBody());
+            String mensagem = new String(delivery.getBody(), StandardCharsets.UTF_8);
             System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+            try {
+                doWork(mensagem);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            } finally {
+                System.out.println("[x] Feito");
+                canal.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
 
         // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
-        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
+        boolean autoAck = false;
+        canal.basicConsume(NOME_FILA, autoAck, callback, consumerTag -> {
             System.out.println("Cancelaram a fila: " + NOME_FILA);
         });
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for(char ch: task.toCharArray()) {
+            if(ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
     }
 }
 
--- src/main/java/Produtor.java
+++ src/main/java/Produtor.java
@@ -1,23 +1,34 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class Produtor {
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("mqadmin");
+        connectionFactory.setPassword("Admin123XX_");
         try (
                 Connection connection = connectionFactory.newConnection();
                 Channel canal = connection.createChannel();
         ) {
-            String mensagem = "Olá";
-            String NOME_FILA = "plica";
+            String mensagem = "Paulo Elias Risucci da Silva ...";
+            String NOME_FILA = "fila_de_tarefas";
 
             //(queue, passive, durable, exclusive, autoDelete, arguments)
-            canal.queueDeclare(NOME_FILA, false, false, false, null);
+            boolean duravel = true;
+            canal.queueDeclare(NOME_FILA, duravel, false, false, null);
 
             // ​(exchange, routingKey, mandatory, immediate, props, byte[] body)
-            canal.basicPublish("", NOME_FILA, false, false, null, mensagem.getBytes());
+            canal.basicPublish(
+                    "",
+                    NOME_FILA,
+                    false,
+                    false,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    mensagem.getBytes());
 
         }
     }
